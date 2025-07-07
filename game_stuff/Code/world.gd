128c544adb31700af58e6c2c1b8e66f29b68b3c9
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2025-07-07 16:31:54 -0400

    made better collision for celebratrors (lack of)

--- game_stuff/Code/world.gd
+++ game_stuff/Code/world.gd
@@ -3,4 +3,6 @@ extends Node2D
 
 func _on_on_the_team_body_entered(body: Node2D) -> void:
 	body.mode = "CELEBRATE"
-	#body.position = Vector2(0,0)
+	body.set_collision_layer_value(1, false)
+	body.set_collision_layer_value(2, false)
+	body.set_collision_mask_value(1, false)
